c957d0a7511e408fa7b049f025e5459cc5bc1634
Marco Calzada <mccalzada@outlook.com>
2021-05-18 22:32:37 -0500

    Adding Main.svelte for handling Index actions

---
+++ src/components/Main.svelte
@@ -0,0 +1,25 @@
+<main class="Main">
+  <div class="Main-container">
+    <div class="Main-content">
+      <slot />
+      <div class="Sidebar">Sidebar</div>
+    </div>
+  </div>
+</main>
+
+<style>
+  .Main-container {
+    display: grid;
+    grid-template-columns: minmax(auto, 1024px);
+    justify-content: center;
+    align-items: center;
+  }
+
+  .Main-content {
+    margin: 2em 0 0 0;
+    display: grid;
+    grid-auto-columns: 3fr 1fr;
+    gap: 60px;
+    justify-content: space-between;
+  }
+</style>
