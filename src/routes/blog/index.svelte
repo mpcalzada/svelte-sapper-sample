64c9e1a9be5176501f0928d4498da7890ca9287f
Marco Calzada <mcalzada@kssoluciones.com>
2021-08-30 17:42:19 -0500

    Añadiendo listado de blogs en el modulo de blogs

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,34 +1,35 @@
 <script context="module">
 	export function preload() {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
+		return this.fetch(`blog.json`)
+			.then((r) => r.json())
+			.then((posts) => {
+				return { posts };
+			});
 	}
 </script>
 
 <script>
+	import Post from '../../components/Post.svelte'
 	export let posts;
 </script>
 
-<style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
-</style>
-
 <svelte:head>
-	<title>Blog</title>
+	<title>Marco Calzada</title>
 </svelte:head>
 
-<h1>Recent posts</h1>
+<div class="Home">
+	<div class="Posts">
+		{#each posts as post}
+			<Post {post} />
+		{/each}
+	</div>
+</div>
 
-<ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
-	{/each}
-</ul>
+<style>
+	.Posts{
+		display: grid;
+		justify-content: space-between;
+		grid-gap: 15px;
+		grid-template-columns: 1fr;
+	}
+</style>
